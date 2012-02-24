bf782d881217c7b3b96e50bcf6d0e3206ffa59cb
f0xx <a.afanasieff@gmail.com>
2012-02-24 22:48:53 +0300

    libswresample makefile added

---
+++ libswresample/Android.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+#LOCAL_APP_ABI := $(APP_ABI)
+LOCAL_ARM_MODE := arm
+
+
+LOCAL_MODULE    := swresample
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(FFMPEG_INCLUDE_DIR)
+
+LOCAL_CFLAGS += $(GLOBAL_CFLAGS)
+LOCAL_CFLAGS += -DHAVE_AV_CONFIG_H  -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -DPIC
+LOCAL_CFLAGS += -fvisibility=hidden
+
+LOCAL_LDFLAGS += $(GLOBAL_LDFLAGS)
+LOCAL_LDFLAGS += -lz -llog
+# -lavcodec -lavutil
+#LOCAL_LDFLAGS += -L$(NDK_APP_DST_DIR) -L$(TARGET_OUT)
+
+LIBSWRESAMPLE_SRC_FILES := \
+		audioconvert.c \
+		rematrix.c \
+		resample.c \
+		swresample.c \
+
+
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+  LOCAL_CFLAGS += -fPIC
+endif
+
+
+LOCAL_SRC_FILES := $(LIBSWRESAMPLE_SRC_FILES)
+
+LOCAL_SHARED_LIBRARIES := avcodec avutil
+
+include $(BUILD_SHARED_LIBRARY)
